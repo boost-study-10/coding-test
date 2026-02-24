618e2cb827408b10d0b2684df890d005a4f85449
ajin <aj5210@naver.com>
2026-02-25 01:15:43 +0900

    [BOJ] 센서 / 골드5 / 15분 https://www.acmicpc.net/problem/2212

---
+++ ajin/0224/2212/2212.js
@@ -0,0 +1,16 @@
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt').toString().split('\n')
+
+const n = Number(input[0])
+const k = Number(input[1])
+const sensor = input[2].split(' ').map(Number).sort((a, b)=> a - b)
+
+const gap = []
+for (let i = 0; i < n - 1; i++) {
+  gap.push(sensor[i + 1] - sensor[i])
+}
+
+gap.sort((a, b) => a - b)
+const centers = gap.slice(0, (n - 1) - (k - 1))
+const answer = centers.reduce((a, b) => a + b, 0) 
+console.log(answer)
+
